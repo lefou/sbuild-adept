eca882c46db03d59d4afd52e3bdcfed1c4e21134
Tobias Roeser <le.petit.fou@web.de>
2013-12-08 10:25:03 +0100

    Tried to explore adept mark-III API, but currently, no usable result.

--- org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/AdeptPlugin.scala
+++ org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/AdeptPlugin.scala
@@ -8,11 +8,12 @@ import de.tototec.sbuild.SchemeHandler
 
 class Adept(val name: String)(implicit project: Project) {
   var schemeName: String = if(name == "") "adept" else name
-
+  // TODO: maintain some kind of alias map, such that we can refer to a preconfigured collection of dependencies by the alias. Think of as "compile" as an alias to all compile dependencies.
 }
 
 class AdeptPlugin(implicit project: Project) extends Plugin[Adept] {
 
+  // TODO: configure adept
   def create(name: String): Adept = new Adept(name)
 
   override def applyToProject(instances: Seq[(String, Adept)]): Unit = instances foreach {
--- org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/internal/AdeptSchemeHandler.scala
+++ org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/internal/AdeptSchemeHandler.scala
@@ -5,6 +5,7 @@ import de.tototec.sbuild.Project
 import de.tototec.sbuild.SchemeHandler.SchemeContext
 import de.tototec.sbuild.SchemeResolver
 import de.tototec.sbuild.TargetContext
+import adept.core.resolution.Resolver
 
 object AdeptSchemeHandler {
   trait Repository
@@ -20,6 +21,25 @@ class AdeptSchemeHandler()(implicit project: Project)
   def resolve(schemeCtx: SchemeContext, targetContext: TargetContext) {
     val requestedDeps = schemeCtx.path.split(",").map(_.trim)
     log.debug("About to resolve the following requested dependencies: " + requestedDeps.mkString(", "))
+
+    // TODO: create a appropriate context loader
+    val variantsLoader = ???
+    val resolver = new Resolver(variantsLoader)
+
+    // TODO: assemble dependencies 
+    val deps = ???
+
+    resolver.resolve(deps) match {
+      case Right(state) =>
+        val ids = state.resolved
+        ids.map { id => 
+        id.value
+        
+        // TODO somehow unpack resolved result, download them and attach to context
+        
+        }
+      case Left(state) => throw new RuntimeException("Could not resolve dependency: " + schemeCtx.fullName + "\nState: " + state)
+    }
   }
 
 }
