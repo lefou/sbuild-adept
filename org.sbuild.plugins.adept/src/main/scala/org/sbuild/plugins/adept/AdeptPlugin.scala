ecf29b49de743c726841dbbf7dc11fea33b1f87b
Tobias Roeser <le.petit.fou@web.de>
2013-12-12 09:59:03 +0100

    Added a target to list resolved dependencies (for testing).

--- org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/AdeptPlugin.scala
+++ org.sbuild.plugins.adept/src/main/scala/org/sbuild/plugins/adept/AdeptPlugin.scala
@@ -1,14 +1,17 @@
 package org.sbuild.plugins.adept
 
 import org.sbuild.plugins.adept.internal.AdeptSchemeHandler
-
 import de.tototec.sbuild.Plugin
 import de.tototec.sbuild.Project
 import de.tototec.sbuild.SchemeHandler
+import de.tototec.sbuild.Target
+import de.tototec.sbuild.Prop
+import de.tototec.sbuild.TargetRef
 
 class Adept(val name: String)(implicit project: Project) {
-  var schemeName: String = if(name == "") "adept" else name
+  var schemeName: String = if (name == "") "adept" else name
   // TODO: maintain some kind of alias map, such that we can refer to a preconfigured collection of dependencies by the alias. Think of as "compile" as an alias to all compile dependencies.
+  var aliases: Seq[(String, Seq[String])] = Seq()
 }
 
 class AdeptPlugin(implicit project: Project) extends Plugin[Adept] {
@@ -18,7 +21,13 @@ class AdeptPlugin(implicit project: Project) extends Plugin[Adept] {
 
   override def applyToProject(instances: Seq[(String, Adept)]): Unit = instances foreach {
     case (name, adept) =>
-      SchemeHandler(name, new AdeptSchemeHandler())
+      SchemeHandler(adept.schemeName, new AdeptSchemeHandler())
+      val listDeps = Seq(Prop("adept.deps", "")).collect { case x if x.size > 0 => TargetRef(s"${adept.schemeName}:${x}") }
+      Target("adept-dependencies") dependsOn listDeps exec {
+        val given = Prop("adept.deps")
+        println("Given: " + given)
+        println("Resolved: " + listDeps.files)
+      } help "Resolve dependencies via adept. The request must be given as property 'adept.deps'."
   }
 
 }
